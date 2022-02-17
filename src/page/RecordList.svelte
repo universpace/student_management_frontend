51a01b552606b5a4d13bd02aced0ef61557a8b39
universpace <woojoo1602>
2022-02-18 00:28:20 +0900

    í기록 목록에 ã 탭추ê¸가

--- src/page/RecordList.svelte
+++ src/page/RecordList.svelte
@@ -1,9 +1,28 @@
 <script>
-    import {Styles} from "sveltestrap";
+    import {Styles, TabContent, TabPane} from "sveltestrap";
 </script>
 <Styles/>
-
+<style>
+    :global(#recordtab){
+        margin-left: 15%;
+        margin-top: 5%;
+    }
+</style>
 <main class="page">
-    <p>RecordList</p>
+    <TabContent id="recordtab">
+        <TabPane tabId="1" tab="1" active>
+            <h2>1</h2>
+        </TabPane>
+        <TabPane tabId="2" tab="2" active>
+            <h2>2</h2>
+        </TabPane>
+        <TabPane tabId="3" tab="3" active>
+            <h2>3</h2>
+        </TabPane>
+        <TabPane tabId="4" tab="4" active>
+            <h2>4</h2>
+        </TabPane>
+    </TabContent>
+
 
 </main>
--- src/page/StudentReg.svelte
+++ src/page/StudentReg.svelte
@@ -1,7 +1,37 @@
 <script>
-    import {Styles} from "sveltestrap";
+    import {Button, Form, FormGroup, Input, Styles} from "sveltestrap";
+    let name = ''
+    let phoneNumber = ''
+    function validateName(n){
+        let patternName=/^[가-힣a-zA-Z]+$/
+        return name.length > 0&&patternName.test(n)
+    }
+    function validatePhoneNumber(n){
+        let patternPhone = /01[016789][^0][0-9]{2,3}[0-9]{3,4}/;
+        return patternPhone.test(n)&&(n.length < 12)
+    }
 </script>
 <Styles/>
+<style>
+    :global(#studentRegForm){
+        margin-left: 25%;
+        margin-top: 20%;
+    }
+</style>
 <main class="page">
-    <p>StudentReg</p>
+    <Form id="studentRegForm" >
+        <FormGroup floating label="학생 이름">
+            <Input placeholder="이름" bsSize="lg" bind:value={name}  invalid={!validateName(name)} valid={validateName(name)}/>
+        </FormGroup>
+        <FormGroup floating label="전화번호">
+            <Input placeholder="-없이 입력해주세요" bind:value={phoneNumber}  invalid={!validatePhoneNumber(phoneNumber)} valid={validatePhoneNumber(phoneNumber)}/>
+        </FormGroup>
+        <FormGroup floating label="주소">
+            <Input placeholder="서울특별시"/>
+        </FormGroup>
+        <FormGroup floating label="상세주소">
+            <Input placeholder="101호"/>
+        </FormGroup>
+        <Button>등록하기</Button>
+    </Form>
 </main>
