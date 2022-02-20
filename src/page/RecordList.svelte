1090bb909b06d0121beef58a80ef604627bc0e10
universpace <woojoo1602>
2022-02-20 15:47:46 +0900

    학생 기록 탭 추가

--- src/page/RecordList.svelte
+++ src/page/RecordList.svelte
@@ -1,28 +1,39 @@
 <script>
-    import {Styles, TabContent, TabPane} from "sveltestrap";
+    import {Form, FormGroup, Icon, Input, InputGroup, InputGroupText, Styles, TabContent, TabPane} from "sveltestrap";
+    let student_name = null
+    let status = 'learning';
 </script>
 <Styles/>
 <style>
     :global(#recordtab){
-        margin-left: 15%;
-        margin-top: 5%;
+        margin-left: 5%;
+
+    }
+    :global(#record-student-input){
+        margin-left: 10%;
+        margin-top: 4%;
     }
 </style>
 <main class="page">
-    <TabContent id="recordtab">
-        <TabPane tabId="1" tab="1" active>
-            <h2>1</h2>
+    <Form id="record-student-input">
+        <InputGroup>
+            <Input placeholder="학생명" bsSize="bg" bind:value={student_name}/>
+            <InputGroupText><Icon name="search"/></InputGroupText>
+        </InputGroup>
+    </Form>
+
+    <TabContent id="recordtab" on:tab={(e) => (status = e.detail)}>
+        <TabPane tabId="learning" tab="학습" active>
+
         </TabPane>
-        <TabPane tabId="2" tab="2" active>
-            <h2>2</h2>
+        <TabPane tabId="life" tab="생활">
+            생활
         </TabPane>
-        <TabPane tabId="3" tab="3" active>
-            <h2>3</h2>
+        <TabPane tabId="tenacity" tab="인성">
+            인성
         </TabPane>
-        <TabPane tabId="4" tab="4" active>
-            <h2>4</h2>
+        <TabPane tabId="remark" tab="특이사항">
+            특이사항
         </TabPane>
     </TabContent>
-
-
 </main>
