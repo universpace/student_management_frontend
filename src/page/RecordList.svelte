3ae680fade6f65c8b2e958931e2f2fe866b5aea0
Kim WooJoo <woojoo1602@gmail.com>
2022-02-20 17:33:42 +0900

    학생 목록에서 검색한 학생 찾아서 가 기록  ¸오기

--- src/page/RecordList.svelte
+++ src/page/RecordList.svelte
@@ -1,7 +1,97 @@
 <script>
-    import {Form, FormGroup, Icon, Input, InputGroup, InputGroupText, Styles, TabContent, TabPane} from "sveltestrap";
-    let student_name = null
+    import {
+        Button,
+        Form,
+        FormGroup,
+        Icon,
+        Input,
+        InputGroup,
+        InputGroupText,
+        Styles,
+        TabContent,
+        TabPane
+    } from "sveltestrap";
+    let studentName = null
     let status = 'learning';
+    let studentList = [
+        {
+            "id": 1,
+            "name": "김우주",
+            "phone_number": "01045342549",
+            "address": "서울특별시 관악구 낙성대역길50",
+            "address_detail": "102호"
+        },
+        {
+            "id": 2,
+            "name": "김갑철",
+            "phone_number": "01011111111",
+            "address": "인천광역시 중구 운서동",
+            "address_detail": "7단지"
+        },
+        {
+            "id": 4,
+            "name": "김갑철",
+            "phone_number": "01011111111",
+            "address": "인천광역시 중구 운서동",
+            "address_detail": "7단지"
+        }
+    ]
+    let recordList = [
+        {
+            "id": 9,
+            "student": 1,
+            "category": "학습",
+            "text": "12345",
+            "hashtag": [
+                "tag1",
+                "tag2"
+            ]
+        },
+        {
+            "id": 8,
+            "student": 1,
+            "category": "생활",
+            "text": "test",
+            "hashtag": [
+                "tag3",
+                "tag4",
+                "tag5"
+            ]
+        },
+        {
+            "id": 10,
+            "student": 2,
+            "category": "학습",
+            "text": "갑테리몬",
+            "hashtag": [
+                "tag7",
+                "tag8"
+            ]
+        }
+    ]
+    let foundStudent = null
+    let selectedRecord = null
+
+    function findStudent(element){
+        if(element.name===studentName){
+            return true
+        }
+        return false
+    }
+    function findRecord(element) {
+        if (element.student === findStudent.id) {
+            return true
+        }
+        return false
+    }
+    function handleSearch(e){
+        console.log(e)
+        if(e.key==='Enter'){
+            foundStudent = studentList.find(findStudent)
+            selectedRecord = recordList.find(findRecord)
+        }
+    }
+
 </script>
 <Styles/>
 <style>
@@ -17,14 +107,18 @@
 <main class="page">
     <Form id="record-student-input">
         <InputGroup>
-            <Input placeholder="학생명" bsSize="bg" bind:value={student_name}/>
-            <InputGroupText><Icon name="search"/></InputGroupText>
+            <Input placeholder="학생명" bsSize="bg" bind:value={studentName} on:keyup="{handleSearch}"/>
+            <InputGroupText><Button color="link"><Icon name="search"/></Button></InputGroupText>
         </InputGroup>
     </Form>
 
     <TabContent id="recordtab" on:tab={(e) => (status = e.detail)}>
         <TabPane tabId="learning" tab="학습" active>
-
+            {#if selectedRecord !== null}
+                <p>{selectedRecord.text}</p>
+            {:else}
+                <p>1</p>
+            {/if}
         </TabPane>
         <TabPane tabId="life" tab="생활">
             생활
